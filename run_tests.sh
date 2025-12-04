e2ff5f5497b9c3173e297e41431e8ac8e0cd25e4
Cedrus Dev Team <dev@cedrus.com>
2025-12-04 21:59:14 +0200

    Fix pytest discovery error by renaming conflicting test file

---
+++ run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to run tests manually when VS Code discovery fails
+
+# Activate virtual environment
+source ./venv/bin/activate
+
+# Run pytest with verbose output
+pytest --verbose
